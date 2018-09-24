b06158af364b35d46aa90c1f0b68e09c63e7ae40
Thanh Bình <36837919+tousdev@users.noreply.github.com>
2018-09-24 20:50:30 +0700

    Update index.js

--- src/Chatible/index.js
+++ src/Chatible/index.js
@@ -22,15 +22,15 @@ export default async (user) => {
     const userDb = await findUser(user.senderId);
     if (!userDb) {
         insertUser(user).then(() => {
-            return Promise.all([findPair(), ChatfuelAPI.sendText(user.senderId, "Hih chờ một chút để mình gửi yêu cầu tìm bạn nhé")]).then(() => {})
+            return Promise.all([findPair(), ChatfuelAPI.sendText(user.senderId, "Đang tìm kiếm đối tượng")]).then(() => {})
         })
     } else if (userDb.status == 0) {
         userRequest(user.senderId).then(() => {
-            return Promise.all([findPair(), ChatfuelAPI.sendText(user.senderId, "Hih chờ một chút để mình gửi yêu cầu tìm bạn nhé")]).then(() => {})
+            return Promise.all([findPair(), ChatfuelAPI.sendText(user.senderId, "Đang tìm kiếm đối tượng")]).then(() => {})
         })
     } else if (userDb.status === 1) {
         if (user.msg.toLowerCase() === "pp") return await byeOne(user.senderId)
-        return await ChatfuelAPI.sendText(user.senderId, "Chờ chút. Yêu cầu của bạn chắc bây giờ không có ai cạ. Thử huỷ đi chọn lại hoặc chờ đợi nhé")
+        return await ChatfuelAPI.sendText(user.senderId, "Chờ chút. Yêu cầu của bạn chắc bây giờ không có ai cả :(. Thử huỷ đi chọn lại hoặc chờ đợi nhé")
     } else if (userDb.idCouple) {
         if (user.msg.toLowerCase() === "pp") return await bye(user.senderId, userDb.idCouple)
         cache.put(user.senderId, userDb.idCouple)
